691e3d7fbcd649b70196220a67cd6fd637fc279f
syn <vicdemilly@gmail.com>
2019-04-11 09:42:37 +0200

    Autocorrection et ajout de commentaires

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function formulaire()
+function compute()
 {
-  let pointure = shoeSize.value;
-  let naissance = yearOfBirth.value;
+  let pointure = document.getElementById('shoeSize').value;
+  let naissance = document.getElementById('yearOfBirth').value;
   alert((pointure * 2 + 5) * 50 - naissance + 1766 );
 }
+// Création de deux viriables d'une valeur donnée puis mise en place d'un calcul automatique avec des nombres pures
