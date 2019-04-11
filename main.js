5ad5744a125ce0555ba093da3430c179cb6f2b2c
syn <vicdemilly@gmail.com>
2019-04-11 16:06:50 +0200

    Ajout d'une condition if isNaN

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function compute()
 {
   let pointure = document.getElementById('shoeSize').value;
   let naissance = document.getElementById('yearOfBirth').value;
-  alert((pointure * 2 + 5) * 50 - naissance + 1766 );
+  //alert((pointure * 2 + 5) * 50 - naissance + 1766 );
+// Création de deux variables d'une valeur donnée puis mise en place d'un calcul automatique avec des nombres purs
+  if (isNaN(pointure) || isNaN(naissance)){
+      alert('Mettez des nombres !')
+  } else {
+    alert((pointure * 2 + 5) * 50 - naissance + 1766)
+  }
 }
-// Création de deux viriables d'une valeur donnée puis mise en place d'un calcul automatique avec des nombres pures
